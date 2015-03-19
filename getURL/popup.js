8b930c8303368454c30cbfc63947c3ab82efb1a9
shenhua889 <shenhua889@126.com>
2015-03-19 22:27:23 +0800

    0.1版本
    现在在chrome插件下能抓取到淘宝页面
    分析出淘宝的搜索页面的items 用json保存
    下一步可以尝试抓取每个item之后排序重新放回json中.应该能实现排序功能

---
+++ getURL/popup.js
@@ -0,0 +1,28 @@
+function renderStatus(statusText) {
+  document.getElementById('status').textContent = statusText;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	getu(function(url){
+	    var request = new XMLHttpRequest();
+	    request.onreadystatechange = function () {
+	        if (request.readyState == 4 && request.status == 200) {
+	            var responseTXT = request.responseText;
+	            renderStatus(url + responseTXT);
+	        }
+	    }
+	    request.open("GET", url, true);
+	    request.send(null);
+	});
+});
+ function getu(callback){
+	var queryInfo = {
+    active: true,
+    currentWindow: true
+	};
+	chrome.tabs.query(queryInfo,function(tabs){
+		var tab=tabs[0];
+		var url=tab.url;
+		callback(url);
+	});
+ }
